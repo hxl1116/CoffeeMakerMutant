7b5cdccef4945d50ea53577152a077e34e7929df
Henry Larson <hxl1116@g.rit.edu>
2019-10-28 21:59:10 -0400

    Added test suites and integrated jUnit 4

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,17 @@
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(AllTests.class.getName());
+        //$JUnit-BEGIN$
+        suite.addTestSuite(CoffeeMakerTest.class);
+        suite.addTestSuite(InventoryTest.class);
+        suite.addTestSuite(RecipeBookTest.class);
+        suite.addTestSuite(RecipeTest.class);
+        //$JUnit-END$
+        return suite;
+    }
+
+}
